79f012adb93bb6979670673c780c0a79645e55eb
TheDim0n <dim0n2023@yandex.ru>
2020-08-14 21:04:39 +0300

    fix

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 
-def create_model():
+def create_model(num_classes=10):
     base_model = ResNet50(
         input_shape=(224, 224, 3),
         include_top=False,
@@ -11,7 +11,7 @@ def create_model():
     inputs = tf.keras.Input(shape=(224, 224, 3))
     x = base_model(inputs, training=False)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    outputs = tf.keras.layers.Dense(CLASSES, activation='softmax')(x)
+    outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
 
     model = tf.keras.Model(inputs, outputs)
     return model
