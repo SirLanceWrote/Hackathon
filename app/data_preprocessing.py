98f1dd33056b9bd87e74f85244d0912d0b9758e1
temchik00 <temchik00@yandex.ru>
2020-08-14 20:52:36 +0300

    Made augmentation

---
+++ app/data_preprocessing.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from PIL import Image
+
+resizeRescale = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.Resizing(224, 224),
+  tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+])
+
+def adjustSaturation(image):
+    return tf.image.adjust_saturation(image, 2)
+
+def adjustQuality(image):
+    return tf.image.adjust_jpeg_quality(image, 50)
+
+def augment(image):
+    tmp = []
+    images = []
+    operations = [tf.image.flip_left_right, tf.image.flip_up_down, adjustSaturation, adjustQuality]
+    image = resizeRescale(image)
+    images.append(image)
+    for operation in operations: 
+        for image in images:
+            tmp.append(operation(image))
+        images += tmp
+        tmp = []
+    return images
+
+def augmentAll(imageLocations = []):
+    dataset = []
+    for imageLocation in imageLocations:
+        image = tf.keras.preprocessing.image.load_img(imageLocation, color_mode='rgb', interpolation='nearest')
+        dataset += augment(image)
+    return tf.data.Dataset.from_tensor_slices(dataset)
