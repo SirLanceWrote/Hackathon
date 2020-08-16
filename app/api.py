dacb3380ed47e25718eaa57e190df1e8cb472a25
TheDim0n <dim0n2023@yandex.ru>
2020-08-16 04:18:25 +0300

    train tuned models

--- app/api.py
+++ app/api.py
@@ -1,17 +1,29 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 
-def create_model(num_classes=10):
-    base_model = ResNet50(
-        input_shape=(224, 224, 3),
-        include_top=False,
-    )
+def addClass(model):
+    to_concat = []
+    base_model = ResNet50(include_top=False)
     base_model.trainable = False
 
     inputs = tf.keras.Input(shape=(224, 224, 3))
-    x = base_model(inputs, training=False)
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    outputs = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+    start = base_model(inputs, training=False)
 
-    model = tf.keras.Model(inputs, outputs)
-    return model
+    pooling = tf.keras.layers.GlobalAveragePooling2D()(start)
+    
+    x = model.layers[-1]
+    name = str(type(x)).split('.')[-1][:-2]
+    if name == 'Concatenate':
+        for layer in model.layers[3:][:-1]:
+            layer.training = False
+            to_concat.append(layer(pooling))
+    else:
+        to_concat.append(x(pooling))
+
+    new_class = tf.keras.layers.Dense(1)(pooling)
+    to_concat.append(new_class)
+
+    concatted = tf.keras.layers.Concatenate()(to_concat)
+    new_model = tf.keras.Model(inputs, concatted)
+
+    return new_model
