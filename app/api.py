7887d309d44d36c08ab7432abd5bc74ad4f880a3
TheDim0n <dim0n2023@yandex.ru>
2020-08-16 07:12:04 +0300

    Auto stash before merge of "develop" and "origin/develop"

--- app/api.py
+++ app/api.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 
-def addClass(model):
+def add_class(model):
+
     to_concat = []
     base_model = ResNet50(include_top=False)
     base_model.trainable = False
+    # model.trainable = False
 
     inputs = tf.keras.Input(shape=(224, 224, 3))
     start = base_model(inputs, training=False)
@@ -18,12 +20,20 @@ def addClass(model):
             layer.training = False
             to_concat.append(layer(pooling))
     else:
+        x.training = False
         to_concat.append(x(pooling))
 
-    new_class = tf.keras.layers.Dense(1)(pooling)
+    new_class = tf.keras.layers.Dense(1, name='Layer'+str(len(to_concat) + 1))(pooling)
     to_concat.append(new_class)
 
     concatted = tf.keras.layers.Concatenate()(to_concat)
     new_model = tf.keras.Model(inputs, concatted)
 
     return new_model
+
+# def train():
+#     model.compile(
+#         optimizer=tf.keras.optimizers.SGD(learning_rate=hp_learning_rate, momentum=hp_momentum, nesterov=True),
+#         loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+#         metrics=['accuracy'],
+#     )
